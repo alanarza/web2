2cd375859acc8c499cdac7b4593e8cb84cbacbd8
alan <arzapersonal@gmail.com>
2016-08-18 11:59:54 -0300

    agregada vista diseño y estilos

--- VISTA_formulario.php
+++ VISTA_formulario.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Formulario</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -18,7 +18,75 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+    <div class="container" style="margin-top: 50px;">
+
+      <div class="col-md-3"></div>
+      
+      <div class="col-md-6">
+
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">Bienvenido al Formulario</h3>
+            </div>
+            <div class="panel-body">
+              <form class="form-horizontal" method="POST" action="controlador.php">
+                <fieldset>
+
+                  <div class="form-group">
+                    <label for="focusedInput" class="col-lg-4 control-label">Nombre y Apellido</label>
+                    <div class="col-lg-8">
+                      <input class="form-control" style="text-align: center;" id="focusedInput" type="text" placeholder="Nombre Apellido">
+                    </div>
+                  </div>
+
+                  <div class="container-fluid">
+
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="focusedInput2" class="col-lg-4 control-label">Edad</label>
+                      <div class="col-lg-8">
+                        <input class="form-control" style="text-align: center;" id="focusedInput2" type="numeric" placeholder="Edad">
+                      </div>
+                    </div>
+                    </div>
+
+                    <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="select" class="col-lg-4 control-label">Pais</label>
+                      <div class="col-lg-8">
+                        <select class="form-control" id="select" >
+
+                          <?php foreach ($paises as $pais): ?>
+
+                            <option><?php echo $pais; ?></option>
+
+                          <?php endforeach;?>
+
+                        </select>
+                      </div>
+                    </div>
+                    </div>
+
+                  </div>
+
+                  <div class="form-group">
+                    <div class="col-lg-10 col-lg-offset-4">
+                      <button type="reset" class="btn btn-default">Limpiar</button>
+                      <button type="submit" class="btn btn-primary">Enviar</button>
+                    </div>
+                  </div>
+
+                </fieldset>
+              </form>
+            </div>
+          </div>
+
+      </div>
+
+      <div class="col-md-3"></div>
+
+    </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<?php 
+<?php
+
+	$paises = [Argentina,Chile,Brazil,Uruguay,Paraguay,Bolivia,Peru];
 
 	require __DIR__.'/VISTA_formulario.php';
